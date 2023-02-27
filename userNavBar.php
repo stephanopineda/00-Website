1bd4900882f816e2cdc9f74a5a7bbde45e8ba8fc
stephanopineda <denniel.step@gmail.com>
2023-02-27 15:19:59 +0800

    nav bar added

---
+++ userNavBar.php
@@ -0,0 +1,9 @@
+<?php
+	echo "<a href = 'index.php'   >Home     </a>
+          <a href = 'products.php'>Products </a>
+          <a href = 'cart.php'    >My Cart  </a>
+          <a href = 'history.php' >History  </a>
+          <a href = 'aboutUs.php' >About Us </a>";
+
+          
+	
