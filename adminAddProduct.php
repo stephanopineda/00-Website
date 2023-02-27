a4e81b3746e0b9ec0a4130af1c3c6fcaea452988
extolhim8 <ernestoilagan08@gmail.com>
2023-02-27 14:41:01 +0800

    more styling

--- adminAddProduct.php
+++ adminAddProduct.php
@@ -9,101 +9,121 @@
     <head>
         <title>Add Product</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <style>
+        html,body{
+            position: absolute;
+            text-align: center;
+            margin: 0;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 25px;
+            border-radius: 25px;
+        }
+
+        .container{
+            text-align: center;
+        }
+    </style>
+
     </head>
     <body>
-        <div>Add Product</div>
-        <br><br>
-        <div>
-            <form method="POST" enctype="multipart/form-data">
-                <input type = 'text'   name = 'productName' placeholder = "Product Name" required><br>
-                <input type = 'text'   name = 'stock'       placeholder = "Stock"        required><br>
-                <input type = 'text'   name = 'price'       placeholder = "Price"        required><br>
-                <input type = 'text'   name = 'description' placeholder = "Description"  required><br>
-                <label for = 'picture'> Product Picture </label>
-                <input type = 'file'   name = "image"     accept=".jpg, .jpeg, .png" />         <br>
-                <input type = 'submit' name = 'add' value = "Add Product">                                          
-                <a href="sAdminDashboard.php">Cancel</a>
-            </form>
-        </div>
-        
-        <!------------------------------------- Scripts ----------------------------------------->
-        <script>
-            // validate password while typing
-        </script>
+        <div class="container">
+            <div>Add Product</div>
+            <br>
+            <div>
+                <form method="POST" enctype="multipart/form-data">
+                    <input type = 'text'   name = 'productName' placeholder = "Product Name" required><br>
+                    <input type = 'text'   name = 'stock'       placeholder = "Stock"        required><br>
+                    <input type = 'text'   name = 'price'       placeholder = "Price"        required><br>
+                    <input type = 'text'   name = 'description' placeholder = "Description"  required><br>
+                    <label for = 'picture'> Product Picture </label>
+                    <input type = 'file'   name = "image"     accept=".jpg, .jpeg, .png" />         <br>
+                    <input type = 'submit' name = 'add' value = "Add Product">                                          
+                    <a href="sAdminDashboard.php">Cancel</a>
+                </form>
+            </div>
+            
+            <!------------------------------------- Scripts ----------------------------------------->
+            <script>
+                // validate password while typing
+            </script>
 
-        <?php
-            if(isset($_POST["add"])) {
-                if($_SESSION["user_type"] === "admin") {
-                    // Image Upload
-                    if(isset($_FILES['image'])){
-                        $errors= array();
-                        $file_name = $_FILES['image']['name'];
-                        $file_size =$_FILES['image']['size'];
-                        $file_tmp =$_FILES['image']['tmp_name'];
-                        $file_type=$_FILES['image']['type'];
-                        $arrayVar = explode('.',$_FILES['image']['name']);
-                        $text = end($arrayVar);
-                        $file_ext=strtolower($text);
-                        $extensions= array("jpeg","jpg","png");
-                
-                        if(in_array($file_ext,$extensions)=== false){
-                            $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-                        }
-                
-                        if($file_size > 2097152){
-                            $errors[]='File size must be excately 2 MB';
-                        }
-                
-                        if(empty($errors)==true){
-                            move_uploaded_file($file_tmp,"images/".$file_name);
-                            echo "Success";
-                        }
-                        else{
-                            print_r($errors);
-                        }
-                    }
+            <?php
+                if(isset($_POST["add"])) {
+                    if($_SESSION["user_type"] === "admin") {
+                        // Image Upload
+                        if(isset($_FILES['image'])){
+                            $errors= array();
+                            $file_name = $_FILES['image']['name'];
+                            $file_size =$_FILES['image']['size'];
+                            $file_tmp =$_FILES['image']['tmp_name'];
+                            $file_type=$_FILES['image']['type'];
+                            $arrayVar = explode('.',$_FILES['image']['name']);
+                            $text = end($arrayVar);
+                            $file_ext=strtolower($text);
+                            $extensions= array("jpeg","jpg","png");
                     
-                    //SQL
-                    $productName =  $_POST['productName'];
-                    $stock = $_POST['stock'];
-                    $price = $_POST['price'];
-                    $description = $_POST['description'];
+                            if(in_array($file_ext,$extensions)=== false){
+                                $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+                            }
                     
-                    $productNameExists = mysqli_query($conn, "SELECT * FROM storeContent WHERE product_name = '".$productName."'");
+                            if($file_size > 2097152){
+                                $errors[]='File size must be excately 2 MB';
+                            }
                     
-                    if((mysqli_num_rows($productNameExists) === 0)) { 
-                        $sql = "INSERT INTO storeContent(product_name, stock, price, description, file_name)
-                            VALUES ('$productName',
-                            '$stock',
-                            '$price',
-                            '$description',
-                            '$file_name')";
-    
-                        if(mysqli_query($conn, $sql)){
-                            echo "
-                                <script>
-                                    alert('Product added.');
-                                    document.location='adminAddProduct.php'
-                                </script>";
+                            if(empty($errors)==true){
+                                move_uploaded_file($file_tmp,"images/".$file_name);
+                                echo "Success";
+                            }
+                            else{
+                                print_r($errors);
+                            }
+                        }
+                        
+                        //SQL
+                        $productName =  $_POST['productName'];
+                        $stock = $_POST['stock'];
+                        $price = $_POST['price'];
+                        $description = $_POST['description'];
+                        
+                        $productNameExists = mysqli_query($conn, "SELECT * FROM storeContent WHERE product_name = '".$productName."'");
+                        
+                        if((mysqli_num_rows($productNameExists) === 0)) { 
+                            $sql = "INSERT INTO storeContent(product_name, stock, price, description, file_name)
+                                VALUES ('$productName',
+                                '$stock',
+                                '$price',
+                                '$description',
+                                '$file_name')";
+        
+                            if(mysqli_query($conn, $sql)){
+                                echo "
+                                    <script>
+                                        alert('Product added.');
+                                        document.location='adminAddProduct.php'
+                                    </script>";
+                            }
+                            else {
+                                echo "ERROR: $sql. " . mysqli_error($conn);
+                            }
                         }
                         else {
-                            echo "ERROR: $sql. " . mysqli_error($conn);
+                            echo "Product already exists.";
                         }
+                        mysqli_close($conn);
                     }
                     else {
-                        echo "Product already exists.";
+                        echo "
+                            <script>
+                                alert('You must login first.');
+                                document.location='signin.php'
+                            </script>
+                        ";
                     }
-                    mysqli_close($conn);
                 }
-                else {
-                    echo "
-                        <script>
-                            alert('You must login first.');
-                            document.location='signin.php'
-                        </script>
-                    ";
-                }
-            }
-        ?>
+            ?>
+        </div>
     </body>
 </html>
--- adminDashboard.php
+++ adminDashboard.php
@@ -9,45 +9,94 @@
     <head>
         <title>Hello <?php echo $_SESSION['first_name']; ?>!</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <style>
+        html,body{
+            height: 100%;
+            margin: 0;
+
+        }
+
+        .container{
+            text-align: center;
+        }
+
+        .table{
+            margin-left: auto;
+            margin-right: auto;
+            border-collapse: collapse;
+
+        }
+
+        th, td {
+            text-align: center;
+            padding: 8px;
+            border: 1px solid black;
+        }
+
+        th {
+            background-color: #dddddd;
+        }
+
+        #add{
+            text-decoration: none;
+            color: black;
+            padding: 10px;
+            margin-top:10px;
+            border:solid 1px #564635;
+            border-radius: 15px;
+            height: 25px;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);
+            width: 250px;
+        }
+
+        #add:hover{
+            background-color: yellow;
+        }
+        
+    </style>
+
     </head>
     <body>
-        <div>
-            Signed in as: <?php echo $_SESSION['first_name']; ?>
-        </div>
-        <div>
-            <a href = 'logout.php'> Logout  </a><br><br><br>
-        </div>
-        <div>
-            <a href = 'adminAddProduct.php'> Add Product </a>
-        </div>
+        <div class="container">
+            <div>
+                Signed in as: <?php echo $_SESSION['first_name']; ?> [ADM]
+            </div>
+            <div>
+                <a href = 'logout.php'> Logout  </a><br><br><br>
+            </div>
+            <div>
+                <a href = 'adminAddProduct.php' id="add"> Add Product </a> 
+            </div> <br> <br>
 
-        <?php
-            $query = "SELECT * FROM storeContent";
-            $result = mysqli_query($conn, $query);
-            
-            echo "<table>";
-            echo "<tr>
-            <th>" . 'file_name'   . "</th>
-            <th>" . 'product_name'. "</th>
-            <th>" . 'stock'       . "</th>
-            <th>" . 'price'       . "</th>
-            <th>" . 'description' . "</th>
-            <th>" . 'action'      . "</th>
-            </tr>";
-
-            while($row = $result->fetch_assoc()) {
+            <?php
+                $query = "SELECT * FROM storeContent";
+                $result = mysqli_query($conn, $query);
+                
+                echo "<table class=table>";
                 echo "<tr>
-                <td><img src='./images/" . $row['file_name']     . "' width = '100px'></td>
-                <td>"  .  htmlspecialchars($row['product_name']) . "</td>
-                <td>"  .  htmlspecialchars($row['stock'])        . "</td>
-                <td>"  .  htmlspecialchars($row['price'])        . "</td>
-                <td>"  .  htmlspecialchars($row['description'])  . "</td>
-                <td><a href = 'edit.php?id=".$row["id"]."' class='btnEdit'>Edit</td>";
-            }
-            
-            echo "</table>";
+                <th>" . 'file_name'   . "</th>
+                <th>" . 'product_name'. "</th>
+                <th>" . 'stock'       . "</th>
+                <th>" . 'price'       . "</th>
+                <th>" . 'description' . "</th>
+                <th>" . 'action'      . "</th>
+                </tr>";
 
-            mysqli_close($conn);
-        ?>
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr>
+                    <td><img src='./images/" . $row['file_name']     . "' width = '100px'></td>
+                    <td>"  .  htmlspecialchars($row['product_name']) . "</td>
+                    <td>"  .  htmlspecialchars($row['stock'])        . "</td>
+                    <td>"  .  htmlspecialchars($row['price'])        . "</td>
+                    <td>"  .  htmlspecialchars($row['description'])  . "</td>
+                    <td><a href = 'edit.php?id=".$row["id"]."' class='btnEdit'>Edit</td>";
+                }
+                
+                echo "</table>";
+
+                mysqli_close($conn);
+            ?>
+        </div>
     </body>
 </html>
