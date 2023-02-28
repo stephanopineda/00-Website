8747f952cbfac1aab08dd6a929568c6a36919e0f
extolhim8 <ernestoilagan08@gmail.com>
2023-02-28 12:48:47 +0800

    index changed

--- userNavBar.php
+++ userNavBar.php
@@ -18,15 +18,19 @@
 </html>
 
 <?php
-	echo "<a href = 'index.php'   >Home     </a>
-          <a href = 'products.php'>Products </a>
-          <a href = 'cart.php'    >My Cart  </a>
-          <a href = 'history.php' >History  </a>
-          <a href = 'aboutUs.php' >About Us </a>";
+	echo "<div class=container>
+            <a href = 'index.php'   >Home     </a>
+            <a href = 'products.php'>Products </a>
+            <a href = 'cart.php'    >My Cart  </a>
+            <a href = 'history.php' >History  </a>
+            <a href = 'aboutUs.php' >About Us </a>
+            </div>";
 
-    if (isset($_SESSION["email"])){
-        echo "<a href = 'profile.php'> Profile  </a>";
-    }
+    // if (isset($_SESSION["email"])){
+    //     echo "<a href = 'profile.php'> Profile  </a>";
+    // }
+    
+    
     
           
 	
