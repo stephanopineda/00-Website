9af4050655165717b671818345405238051486b4
stephanopineda <denniel.step@gmail.com>
2023-02-28 12:37:29 +0800

    profile link

--- userNavBar.php
+++ userNavBar.php
@@ -4,10 +4,4 @@
           <a href = 'cart.php'    >My Cart  </a>
           <a href = 'history.php' >History  </a>
           <a href = 'aboutUs.php' >About Us </a>";
-
-    if (isset($_SESSION["email"])){
-        echo "<a href = 'profile.php'> Profile  </a>";
-    }
-    
-          
 	
