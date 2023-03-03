745ea19db062e4111af704df6018163609a29e41
extolhim8 <124751379+extolhim8@users.noreply.github.com>
2023-03-03 09:58:37 +0800

    .

--- adminOrderShipped.php
+++ adminOrderShipped.php
@@ -3,12 +3,28 @@
     include 'userRedirect.php';
     include 'sAdminRedirect.php';
 
-    $id = intval($_GET['id']);
+    $orderId = intval($_GET['id']);
+    $sqlOrder = "SELECT product_id, quantity FROM orders WHERE id = $orderId";
+    $order = mysqli_query($conn, $sqlOrder);
+    $orderRow = $order->fetch_assoc();
+    $product_id = $orderRow['product_id'];
+    $orderQuantity = $orderRow['quantity'];
+
+    $sqlProd = "SELECT stock FROM storeContent WHERE id = $product_id";
+    $prod = mysqli_query($conn, $sqlProd);
+    $prodRow = $prod->fetch_assoc();
+    $prevStock = $prodRow['stock'];
+    $newStock = $prevStock - $orderQuantity;
 
     $sql = "UPDATE orders 
             SET order_status = 'To be delivered'
-            WHERE id = $id";
+            WHERE id = $orderId";
+            
     if(mysqli_query($conn, $sql)) {
+        $sqlUpdateQuantity = "UPDATE storeContent 
+                            SET stock = $newStock
+                            WHERE id = $product_id";
+        mysqli_query($conn, $sqlUpdateQuantity);
         echo "
             <script>
                 alert('Order Shipped.');
