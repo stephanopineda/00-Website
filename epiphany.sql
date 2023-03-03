eef05ff25ff3cd121a61a818ce3f3d55061cd4c5
extolhim8 <124751379+extolhim8@users.noreply.github.com>
2023-03-03 09:34:28 +0800

    placeOrder

--- epiphany.sql
+++ epiphany.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2023 at 02:18 PM
+-- Generation Time: Mar 03, 2023 at 02:02 AM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,15 +42,12 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `user_id`, `product_id`, `quantity`, `date_added`, `order_status`, `date_received`) VALUES
-(17, 38, 12, 1, '2023-03-01 13:09:12', 'Received', '2023-03-01 22:34:25'),
-(18, 38, 17, 1, '2023-03-01 13:09:12', 'Received', '2023-03-01 22:26:04'),
-(19, 38, 16, 1, '2023-03-02 06:57:25', 'Received', '2023-03-02 14:58:13'),
-(21, 38, 15, 1, '2023-03-02 07:10:39', 'Received', '2023-03-02 15:21:52'),
-(24, 38, 16, 1, '2023-03-02 07:39:15', 'Received', '2023-03-02 16:06:26'),
-(28, 38, 15, 3, '2023-03-02 07:59:30', 'Received', '2023-03-02 16:06:28'),
-(29, 38, 17, 2, '2023-03-02 08:03:03', 'Received', '2023-03-02 16:06:29'),
-(32, 38, 15, 5, '2023-03-02 09:38:31', 'Pending', NULL),
-(33, 38, 12, 1, '2023-03-02 09:38:31', 'Pending', NULL);
+(42, 46, 21, 1, '2023-03-02 15:58:06', 'Received', '2023-03-03 02:27:50'),
+(43, 46, 22, 15, '2023-03-02 18:25:36', 'To be delivered', NULL),
+(44, 46, 20, 7, '2023-03-02 19:25:54', 'Pending', NULL),
+(45, 46, 21, 1, '2023-03-02 19:25:54', 'Pending', NULL),
+(46, 46, 22, 1, '2023-03-02 19:25:54', 'To be delivered', NULL),
+(47, 48, 22, 8, '2023-03-03 00:06:42', 'Received', '2023-03-03 08:08:56');
 
 -- --------------------------------------------------------
 
@@ -77,16 +74,11 @@ CREATE TABLE `registeredusers` (
 --
 
 INSERT INTO `registeredusers` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `date_registered`, `user_type`, `phone_num`, `address`, `birthdate`) VALUES
-(28, 'Stephano', 'Pineda', 'stephanopineda', 'denniel.step@gmail.com', 'asd', '2023-02-22 19:22:40', 'user', 9953853832, '', '2001-03-24'),
-(29, 'Stephano', 'Pineda', 'adminako', 's@gmail.com', 'asd', '2023-02-22 19:22:40', 'sAdmin', 9953853832, '', NULL),
-(38, 'sha', 'bu', 'shabu', 'sh@bu.com', 'S', '2023-02-24 14:18:57', 'user', 9953853832, '', NULL),
-(39, 'nice', 'nice', 'nice', 'a@aas.com', 'aa', '2023-02-24 17:24:30', 'admin', 0, '', NULL),
-(40, 'abdul', 'admin', 'adminabdul', 'admin@bd.ul', 'yes', '2023-02-24 19:11:12', 'admin', 0, '', NULL),
-(41, 'Stephano', 's', 's', 's@s.c', 'ugFrh6Wzy88A5r9', '2023-03-02 16:17:32', 'user', 9234567890, '', NULL),
-(42, 'sha', 's', 'daas', 's@s.ca', 'asd', '2023-03-02 16:21:42', 'user', 9234567890, '', NULL),
-(43, 'asd', 'asd', 'ssf', 'd@asd.com', 'asd', '2023-03-02 20:21:18', 'user', 9953853832, '', NULL),
-(44, 'asds', 'asa', 'gga', 'gas@ga.c', 'yes', '2023-03-02 20:22:48', 'user', 9953853832, '', NULL),
-(45, 'te', 'te', 'te', 'te@te.mo', 'ass', '2023-03-02 20:28:46', 'admin', 0, '', NULL);
+(16, 'Ernesto IV', 'Ilagan', 'extolhim8', 'ernestoilagan08@gmail.com', 'pass123', '2023-03-02 21:39:30', 'admin', 9167813909, 'Road 5, Brgy. Bagong Silangan, QC', '2001-09-15'),
+(17, 'ERNESTO IV', 'ILAGAN', 'admin1', 'ernesto@gmail.com', 'admin1', '2023-03-02 21:42:00', 'sAdmin', 2418523457, 'Road X, Brgy. Bagong Silangan, QC', '2001-09-16'),
+(29, 'Stephano', 'Pineda', 'adminako', 's@gmail.com', 'asd', '2023-02-22 19:22:40', 'sAdmin', 9953853832, 'QC', '2000-12-12'),
+(46, 'Anthony', 'Ilagan', 'anthony', 'anthony@gmail.com', 'anthony', '2023-03-02 21:42:46', 'user', 9356781542, 'Road 6', '2023-03-06'),
+(48, 'Joshua', 'Ilagan', 'joshua', 'joshua@gmail.com', 'joshua', '2023-03-03 08:06:14', 'user', 9657458211, '', NULL);
 
 -- --------------------------------------------------------
 
@@ -101,6 +93,13 @@ CREATE TABLE `shopping_cart` (
   `quantity` int(11) UNSIGNED NOT NULL,
   `date_added` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `shopping_cart`
+--
+
+INSERT INTO `shopping_cart` (`id`, `user_id`, `product_id`, `quantity`, `date_added`) VALUES
+(62, 46, 24, 25, '2023-03-02 20:07:35');
 
 -- --------------------------------------------------------
 
@@ -122,10 +121,11 @@ CREATE TABLE `storecontent` (
 --
 
 INSERT INTO `storecontent` (`id`, `product_name`, `description`, `stock`, `price`, `file_name`) VALUES
-(12, 'laplace', 'transform', 90, '500.00', '320410350_5696086537172108_8210637093430714788_n.jpg'),
-(15, 'dog', 'iro', 123, '123.00', 'c90a15f1c67863ea32846f5f14dc4897.jpg'),
-(16, 'shiba inu', 'dog', 123, '11.00', '0305a6654ff5fa22e6e47d5d1d1b4d13.jpg'),
-(17, 'pine tree', 'meme', 12121, '111.00', '311889924_586055786652955_3998794895178176438_n.jpg');
+(20, 'Epiphany Pumpkin Spice', 'Pumpkin, Citrus and Woody', 500, '200.00', 'prod1.jpg'),
+(21, 'Epiphany Sweet Pea', 'Sweet, Floral and Fresh', 5, '500.00', 'prod2.jpg'),
+(22, 'Epiphany Morning Dew', 'Floral & Citrus', 95, '400.00', 'prod3.jpg'),
+(23, 'Epiphany Honey Bunch', 'Rose and Oriental', 400, '999.00', 'prod5.jpg'),
+(24, 'Epiphany Snow Punch', 'Aromatic Vanilla', 9975, '60.00', 'prod4.jpg');
 
 --
 -- Indexes for dumped tables
@@ -167,25 +167,25 @@ ALTER TABLE `storecontent`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `registeredusers`
 --
 ALTER TABLE `registeredusers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `shopping_cart`
 --
 ALTER TABLE `shopping_cart`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `storecontent`
 --
 ALTER TABLE `storecontent`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Constraints for dumped tables
--- placeOrder.php
+++ placeOrder.php
@@ -3,67 +3,83 @@
     include 'adminRedirect.php';
     include 'sAdminRedirect.php';
 
-    $sqlUserCart = "SELECT * FROM shopping_cart WHERE user_id = $user_id";
-    $userCart = mysqli_query($conn, $sqlUserCart);
+    $sqlUserProfile = "SELECT address FROM registeredUsers WHERE id = $user_id";
+    $userProfile = mysqli_query($conn, $sqlUserProfile);
+    $userProfileRow = $userProfile->fetch_assoc();
 
-    if($_SESSION["user_type"] === "user") {
-        if((mysqli_num_rows($userCart) != 0)) {
-            while($userCartRow = $userCart->fetch_assoc()) {
-                $user_id = $userCartRow['user_id'];
-                $product_id = $userCartRow['product_id'];
-                $quantity = $userCartRow['quantity'];
-                $order_status = 'Pending';
+    if ($userProfileRow['address'] != NULL){
+        $sqlUserCart = "SELECT * FROM shopping_cart WHERE user_id = $user_id";
+        $userCart = mysqli_query($conn, $sqlUserCart);
+    
+        if($_SESSION["user_type"] === "user") {
+            if((mysqli_num_rows($userCart) != 0)) {
+                while($userCartRow = $userCart->fetch_assoc()) {
+                    $user_id = $userCartRow['user_id'];
+                    $product_id = $userCartRow['product_id'];
+                    $quantity = $userCartRow['quantity'];
+                    $order_status = 'Pending';
+                    
+                    // Check if another same product is pending
+                    $sqlOrder = "SELECT * FROM orders
+                                WHERE user_id = '$user_id' && product_id = '$product_id' && order_status = 'pending'";
+                    $orderExists = mysqli_query($conn, $sqlOrder);
+    
+                    if((mysqli_num_rows($orderExists) == 1)){
+                        // Add quant
+                        $row = $orderExists->fetch_assoc();
+                        $orderQuantity = $row['quantity'] + $quantity;
+                        $id = $row['id'];
+                        $sql = "UPDATE orders 
+                                SET quantity = $orderQuantity
+                                WHERE id = '$id'";
+                    }
+                    else{
+                        // Insert if product is not pending
+                        $sql = "INSERT INTO orders(user_id, product_id, quantity, date_added, order_status)
+                                VALUES ('$user_id', '$product_id', '$quantity', CURRENT_TIMESTAMP(), '$order_status')";
+                    }
                 
-                // Check if another same product is pending
-                $sqlOrder = "SELECT * FROM orders
-                            WHERE user_id = '$user_id' && product_id = '$product_id' && order_status = 'pending'";
-                $orderExists = mysqli_query($conn, $sqlOrder);
-
-                if((mysqli_num_rows($orderExists) == 1)){
-                    // Add quant
-                    $row = $orderExists->fetch_assoc();
-                    $orderQuantity = $row['quantity'] + $quantity;
-                    $id = $row['id'];
-                    $sql = "UPDATE orders 
-                            SET quantity = $orderQuantity
-                            WHERE id = '$id'";
-                }
-                else{
-                    // Insert if product is not pending
-                    $sql = "INSERT INTO orders(user_id, product_id, quantity, date_added, order_status)
-                            VALUES ('$user_id', '$product_id', '$quantity', CURRENT_TIMESTAMP(), '$order_status')";
-                }
-            
-                if(mysqli_query($conn, $sql)) {
-                    $sqlRemove = "DELETE FROM shopping_cart WHERE product_id = $product_id && user_id = $user_id";
-                    mysqli_query($conn, $sqlRemove);
-                    echo "
-                        <script>
-                            alert('Order Placed.');
-                            document.location='orders.php'
-                        </script>
-                    ";
-                }
-                else {
-                    echo "ERROR: $sql. "
-                        . mysqli_error($conn);
+                    if(mysqli_query($conn, $sql)) {
+                        $sqlRemove = "DELETE FROM shopping_cart WHERE product_id = $product_id && user_id = $user_id";
+                        mysqli_query($conn, $sqlRemove);
+                        echo "
+                            <script>
+                                alert('Order Placed.');
+                                document.location='orders.php'
+                            </script>
+                        ";
+                    }
+                    else {
+                        echo "ERROR: $sql. "
+                            . mysqli_error($conn);
+                    }
                 }
             }
+            else{
+                echo "
+                <script>
+                    alert('Cart is empty.');
+                    document.location='cart.php'
+                </script>
+            ";
+            }
         }
-        else{
+        else {
             echo "
-            <script>
-                alert('Cart is empty.');
-                document.location='cart.php'
-            </script>
-        ";
+                <script>
+                    alert('You must login first.');
+                    document.location='signin.php'
+                </script>
+            ";
         }
     }
-    else {
+    else{
         echo "
-            <script>
-                alert('You must login first.');
-                document.location='signin.php'
-            </script>
-        ";
+                <script>
+                    alert('Add an address before placing order.');
+                    document.location='profile.php? id=".$user_id."'
+                </script>
+            ";
     }
+
+    
